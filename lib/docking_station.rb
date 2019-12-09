c620ff0af473d5753a3abc566133cfba769b929f
Charlotte Mannion <charlottemannion100@gmail.com>
2019-12-09 12:03:59 +0000

    TDDs creation initialization of docking station with a bike, but test not yet passing. Returns a real instance of bike class as opposed to mock

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,19 @@
+require_relative 'bike'
+
 class DockingStation
+
+  attr_accessor :bike
+
+  def initialize(bike_class = Bike)
+    @bike_class = bike_class
+    @bike  = Bike.new
+  end
 
   def release_bike
   end
 
 end
+
+# p bike = Bike.new
+p dst = DockingStation.new
+p dst.bike
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,18 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
     it { is_expected.to respond_to "release_bike" }
+
+    describe '#initialize' do
+      it 'should have a bike' do
+        bike_double = double :bike
+        bike_class_double = double :bike_class, new: bike_double
+        dst = DockingStation.new(bike_class_double)
+        expect(dst.bike).to eq(bike_double)
+    end
+  end
+
 end
 
 # require './lib/docking_station.rb'
