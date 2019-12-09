12251e107ff54053eac743dfb9a5e2fa153a38e1
Charlotte Mannion <charlottemannion100@gmail.com>
2019-12-09 13:42:08 +0000

    writes intialize method that creates a working bike

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,4 +2,17 @@ require 'bike'
 
 describe Bike do
   it { is_expected.to respond_to "working?" }
+
+  describe '#initialize' do
+    it 'is working when the bike is created' do
+      bike = Bike.new
+      expect(bike.working?).to eq(true)
+    end
+  end
+
+  describe '#working?' do
+    it 'returns true if the bike is working' do
+      bike = Bike.new
+    end
+  end
 end
